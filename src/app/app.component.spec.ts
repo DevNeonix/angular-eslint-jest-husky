44df8204ddee91b6d283a2b961952708773961e9
Jolver Andre Gil Quevedo <jolveragq@ext.massimodutti.com>
2023-09-01 23:21:48 +0200

    feat: fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [RouterModule],
     }).compileComponents();
   });
 
